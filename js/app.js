e6615983fcb6a63a9350a9da8c528ce0a30e3076
peter2336 <yuchehsu2336@gmail.com>
2024-10-29 10:08:43 +0800

    Add search filter feature

--- js/app.js
+++ js/app.js
@@ -124,3 +124,21 @@ function addMemo() {
   let memoBox = document.getElementById("memoBox");
   memoBox.appendChild(memo);
 }
+
+//search filter
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  const memos = document.querySelectorAll("[id^=memo-]");
+  memos.forEach((memo) => {
+    const memoValue = memo.querySelector("input").value;
+    if (!memoValue.includes(searchTerm)) {
+      memo.classList.remove("d-flex");
+      memo.classList.add("d-none");
+    } else {
+      memo.classList.remove("d-none");
+      memo.classList.add("d-flex");
+    }
+  });
+});
