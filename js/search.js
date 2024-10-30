aacf1b8580404dfc9ee902f463eacae874a9d020
peter2336 <yuchehsu2336@gmail.com>
2024-10-30 16:52:43 +0800

    Add search feature

--- js/search.js
+++ js/search.js
@@ -3,6 +3,8 @@ const searchInput = document.getElementById("searchInput");
 searchInput.addEventListener("input", () => {
   const searchTerm = searchInput.value.toLowerCase();
   const memos = document.querySelectorAll("[id^=memo-]");
+  const notes = document.querySelectorAll("[id^=note-]");
+
   memos.forEach((memo) => {
     const memoValue = memo.querySelector("input").value.toLowerCase();
     if (!memoValue.includes(searchTerm)) {
@@ -11,6 +13,17 @@ searchInput.addEventListener("input", () => {
     } else {
       memo.classList.remove("d-none");
       memo.classList.add("d-flex");
+    }
+  });
+
+  notes.forEach((note) => {
+    const noteValue = note.querySelector("textarea").value.toLowerCase();
+    if (!noteValue.includes(searchTerm)) {
+      note.classList.remove("d-flex");
+      note.classList.add("d-none");
+    } else {
+      note.classList.remove("d-none");
+      note.classList.add("d-flex");
     }
   });
 });
